ac74263c7d9c41952e86170aefc0a66ba9b26f83
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-11 15:26:20 +0530

    Added scripts

---
+++ 04-Adding-notification-status-and-handling-error-state-on-put-http/src/components/Cart/CartButton.js
@@ -0,0 +1,23 @@
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch, useSelector } from 'react-redux';
+
+const CartButton = (props) => {
+  // below lone gives access to dispatch function provided by redux
+  const dispatch = useDispatch();
+
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
